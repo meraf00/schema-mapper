7b8941c3a7eb034d4d361042208f46bcf1e7c9cb
Leul Wujira <meraf000@gmail.com>
2024-04-16 15:36:21 +0300

    feat: add schema entities

---
+++ src/schema/entities/attribute.entity.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+import { Table } from './table.entity';
+
+export enum AttributeType {
+  CHAR = 'CHAR',
+  VARCHAR = 'VARCHAR',
+  TEXT = 'TEXT',
+  BOOLEAN = 'BOOLEAN',
+  TINYINT = 'TINYINT',
+  INTEGER = 'INTEGER',
+  BIGINT = 'BIGINT',
+  FLOAT = 'FLOAT',
+  DOUBLE = 'DOUBLE',
+  DATE = 'DATE',
+  TIME = 'TIME',
+  TIMESTAMP = 'TIMESTAMP',
+}
+
+@Entity()
+@Unique(['name', 'tableId'])
+export class Attribute {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: AttributeType,
+    default: AttributeType.VARCHAR,
+  })
+  type: AttributeType;
+
+  @Column({ default: false })
+  isNullable: boolean;
+
+  @Column({ default: false })
+  isUnique: boolean;
+
+  @Column({ default: false })
+  isPrimary: boolean;
+
+  @Column({ default: false })
+  isForeign: boolean;
+
+  @Column({ default: false })
+  isGenerated: boolean;
+
+  @OneToOne(() => Attribute, { nullable: true })
+  references: Attribute;
+
+  @ManyToOne(() => Table, (table) => table.attributes, { nullable: false })
+  table: Table;
+
+  @Column()
+  tableId: string;
+}
---
+++ src/schema/entities/index.ts
@@ -0,0 +1,3 @@
+export { Schema } from './schema.entity';
+export { Table } from './table.entity';
+export { Attribute, AttributeType } from './attribute.entity';
