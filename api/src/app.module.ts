808b6296910de90937ad3f55fdf3d3d1d7aad712
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-05-24 23:51:59 +0300

    feat: add env variables & rename schema module to project

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,6 @@
 // docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
 import { Module } from '@nestjs/common';
-import { SchemaModule } from './schema/schema.module';
+import { SchemaModule } from './project/project.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
 import { GeneratorModule } from './generator/generator.module';
@@ -10,11 +10,13 @@ import { BullModule } from '@nestjs/bull';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'schema_mapper',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT
+        ? Number(process.env.DATABASE_PORT)
+        : 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts}'],
       synchronize: true,
       autoLoadEntities: true,
@@ -22,8 +24,8 @@ import { BullModule } from '@nestjs/bull';
 
     BullModule.forRoot({
       redis: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
       },
     }),
 
